e9ab351bf73f7cc06bb65171abf5d2181671909e
Benson Chu <bensonchu457@gmail.com>
2019-06-16 21:25:40 -0500

    Intitial repo setup

---
+++ exwm-background.el
@@ -0,0 +1,30 @@
+;;; exwm-background.el --- Allow sending X windows to the background in EXWM -*- lexical-binding: t; -*-
+
+;; Copyright 2019 Benson Chu <bensonchu457@gmail.com>
+
+;; Author: Benson Chu <bensonchu457@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((exwm "22.1"))
+;; URL: https://github.com/pestctrl/exwm-background
+;; Keywords: tools
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; I saw an internet post online about someone wanting to watch youtube
+;; videos or read articles behind EXWM via transparency, and I wanted
+;; that as well. Thus, I wrote this package. 
+
+;;; Code:
